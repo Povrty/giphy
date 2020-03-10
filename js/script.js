bbd5134c3376a28b1ec18d950a46a65e27cbe824
Povrty <iakwan@s.sfusd.edu>
2020-03-10 01:26:06 +0000

    Finished Day 1 & Day 2

--- js/script.js
+++ js/script.js
@@ -4,7 +4,20 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+  var userInput = $("#search-term").val();
+  var request_url = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&rating=pg&api_key=DZv24irnixTUp5tlcJ8hv8eiQHdYBdmM";
+  var random = Math.random()*25;
+  var randomizer = Math.floor(random);
+  fetch(request_url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var pic_url = data.data[randomizer].images.original.url;
+      console.log(random);
+        $('.gallery').html("<div class='text-center'><img src=" + pic_url + "></div>");
+        $('#msg').hide();
+    })  
   
   
 });
